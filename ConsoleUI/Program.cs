cfd349e127391ab3703f851251f19a0d07613841
Harun <Harun@DESKTOP-6D5U418>
2022-02-27 20:28:08 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+//SOLID
+//Open Close Principle
+
+ProductManager productManager = new ProductManager(new EfProductDal());
+foreach (var product in productManager.GetByUnitPrice(50,100))
+{
+    Console.WriteLine(product.ProductName);
+}
+
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //Context db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}

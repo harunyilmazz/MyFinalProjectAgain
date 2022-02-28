a6810a6a7821f4523e31fcde1040208c02172e03
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-02-28 19:16:20 +0300

    DTO eklendi.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,20 +7,25 @@ using DataAccess.Concrete.InMemory;
 //SOLID
 //Open Close Principle
 
-//ProductTest();
+//Data Transformation Object
+ProductTest();
 
-CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-foreach (var category in categoryManager.GetAll())
-{
-    Console.WriteLine(category.CategoryName);
-}
+//CategoryTest();
 
 static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
-    foreach (var product in productManager.GetByUnitPrice(50, 100))
+    foreach (var product in productManager.GetProductDetails())
     {
-        Console.WriteLine(product.ProductName);
+        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
     }
 }
 
+static void CategoryTest()
+{
+    CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+    foreach (var category in categoryManager.GetAll())
+    {
+        Console.WriteLine(category.CategoryName);
+    }
+}

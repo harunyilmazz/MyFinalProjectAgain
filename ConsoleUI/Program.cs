7ff8dbd044568778c6ef7c7aa832eb9df173d4cf
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-03-13 16:41:15 +0300

    Resultlar eklendi

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -15,10 +15,22 @@ ProductTest();
 static void ProductTest()
 {
     ProductManager productManager = new ProductManager(new EfProductDal());
-    foreach (var product in productManager.GetProductDetails())
+
+    var result = productManager.GetProductDetails();
+
+    if (result.Success==true)
     {
-        Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+        foreach (var product in productManager.GetProductDetails().Data)
+        {
+            Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+        }
     }
+    else
+    {
+        Console.WriteLine(result.Message);
+    }
+
+    
 }
 
 static void CategoryTest()

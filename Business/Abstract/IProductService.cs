27bb3f1cb01accd7359b197f7f1079d9e9e1b303
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-03-14 19:57:06 +0300

    WebAPI entegre edildi

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -17,5 +17,7 @@ namespace Business.Abstract
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
+
+        //RESTFULL --> HTTP --> 
     }
 }
--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -35,7 +35,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==15)
+            if (DateTime.Now.Hour==18)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }

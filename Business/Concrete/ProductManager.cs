c435c449442bf1502bddb3391d606c7543cb13b1
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-03-17 16:43:21 +0300

    SecuredOperation altyapısı eklendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -27,6 +27,8 @@ namespace Business.Concrete
             _categoryService= categoryService;
         }
 
+        //Claim
+        //[SecuredOperation("product.add")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {

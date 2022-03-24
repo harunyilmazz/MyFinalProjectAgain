da3c8312778d4487079e12ea67f677364905c748
Harun Yılmaz <harunyilmaaz@gmail.com>
2022-03-24 23:29:43 +0300

    MemoryCache altyapısı eklendi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -44,6 +44,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductAdded);
         }
 
+        //[CacheAspect]  //key, value
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour==18)
